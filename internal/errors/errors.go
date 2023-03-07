1f87b14a2d47e3414f9a472289f382880a160bb0
fatihsalli <mimsallifatih@gmail.com>
2023-03-07 10:48:12 +0300

    custom-errors

---
+++ internal/errors/errors.go
@@ -0,0 +1,17 @@
+package errors
+
+type InternalServerError struct {
+	Message string
+}
+
+type NotFoundError struct {
+	Message string
+}
+
+type BadRequestError struct {
+	Message string
+}
+
+type ClientSideError struct {
+	Message string
+}
