d19df7c23627a3974e685c9b239ae0d0cb0f19ee
fatihsalli <mimsallifatih@gmail.com>
2023-03-07 12:36:46 +0300

    dtos

--- internal/app/dtos.go
+++ internal/app/dtos.go
@@ -1,5 +1,20 @@
 package app
 
+type UserCreateRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Address  string `json:"address"`
+}
+
+type UserUpdateRequest struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Address  string `json:"address"`
+}
+
 type UserResponse struct {
 	ID       string        `json:"id"`
 	Name     string        `json:"name"`
@@ -7,6 +22,27 @@ type UserResponse struct {
 	Password string        `json:"password"`
 	Address  string        `json:"address"`
 	Orders   OrderResponse `json:"orders"`
+}
+
+type OrderCreateRequest struct {
+	UserId  string `json:"user_id"`
+	Product []struct {
+		Name     string  `json:"name"`
+		Quantity int     `json:"quantity"`
+		Price    float64 `json:"price"`
+	}
+	Total float64 `json:"total"`
+}
+
+type OrderUpdateRequest struct {
+	ID      string `json:"id"`
+	UserId  string `json:"user_id"`
+	Product []struct {
+		Name     string  `json:"name"`
+		Quantity int     `json:"quantity"`
+		Price    float64 `json:"price"`
+	}
+	Total float64 `json:"total"`
 }
 
 type OrderResponse struct {
