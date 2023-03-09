77f4cc9a567136211ddd8e43c074ed4ca3ee1b0b
fatihsalli <mimsallifatih@gmail.com>
2023-03-09 16:39:12 +0300

    order-api

--- internal/apps/order-api/handler/handler.go
+++ internal/apps/order-api/handler/handler.go
@@ -6,6 +6,7 @@ import (
 	"OrderUserProject/pkg"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
 	"log"
 	"net/http"
 )
@@ -20,7 +21,9 @@ func NewOrderHandler(e *echo.Echo, service order_api.IOrderService) *OrderHandle
 
 	//Routes
 	router.GET("", b.GetAllOrders)
+	router.GET("/:id", b.GetOrderById)
 	router.POST("", b.CreateOrder)
+	router.PUT("", b.UpdateOrder)
 	router.DELETE("/:id", b.DeleteOrder)
 
 	return b
@@ -63,6 +66,50 @@ func (h OrderHandler) GetAllOrders(c echo.Context) error {
 	}
 
 	log.Print("All books are listed.")
+	return c.JSON(http.StatusOK, jsonSuccessResultData)
+}
+
+// GetOrderById godoc
+// @Summary get a order item by ID
+// @ID get-order-by-id
+// @Produce json
+// @Param id path string true "order ID"
+// @Success 200 {object} models.JSONSuccessResultData
+// @Success 404 {object} pkg.NotFoundError
+// @Success 500 {object} pkg.InternalServerError
+// @Router /orders/{id} [get]
+func (h OrderHandler) GetOrderById(c echo.Context) error {
+	query := c.Param("id")
+
+	order, err := h.Service.GetOrderById(query)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			log.Printf("Not found exception: {%v} with id not found!", query)
+			return c.JSON(http.StatusNotFound, pkg.NotFoundError{
+				Message: fmt.Sprintf("Not found exception: {%v} with id not found!", query),
+			})
+		}
+		log.Printf("StatusInternalServerError: %v", err.Error())
+		return c.JSON(http.StatusInternalServerError, pkg.InternalServerError{
+			Message: "Something went wrong!",
+		})
+	}
+
+	// we can use automapper, but it will cause performance loss.
+	var orderResponse order_api.OrderResponse
+	orderResponse.ID = order.ID
+	orderResponse.UserId = order.UserId
+	orderResponse.Product = order.Product
+	orderResponse.Total = order.Total
+
+	// to response success result data => single one
+	jsonSuccessResultData := models.JSONSuccessResultData{
+		TotalItemCount: 1,
+		Data:           orderResponse,
+	}
+
+	log.Printf("{%v} with id is listed.", orderResponse.ID)
 	return c.JSON(http.StatusOK, jsonSuccessResultData)
 }
 
@@ -111,6 +158,61 @@ func (h OrderHandler) CreateOrder(c echo.Context) error {
 
 	log.Printf("{%v} with id is created.", jsonSuccessResultId.ID)
 	return c.JSON(http.StatusCreated, jsonSuccessResultId)
+}
+
+// UpdateOrder godoc
+// @Summary update an item to the order list
+// @ID update-order
+// @Produce json
+// @Param data body order_api.OrderUpdateRequest true "order data"
+// @Success 200 {object} models.JSONSuccessResultId
+// @Success 400 {object} pkg.BadRequestError
+// @Success 500 {object} pkg.InternalServerError
+// @Router /orders [put]
+func (h OrderHandler) UpdateOrder(c echo.Context) error {
+
+	var orderUpdateRequest order_api.OrderUpdateRequest
+
+	// we parse the data as json into the struct
+	if err := c.Bind(&orderUpdateRequest); err != nil {
+		log.Printf("Bad Request! %v", err)
+		return c.JSON(http.StatusBadRequest, pkg.BadRequestError{
+			Message: fmt.Sprintf("Bad Request. It cannot be binding! %v", err.Error()),
+		})
+	}
+
+	if _, err := h.Service.GetOrderById(orderUpdateRequest.ID); err != nil {
+		log.Printf("Not found exception: {%v} with id not found!", orderUpdateRequest.ID)
+		return c.JSON(http.StatusNotFound, pkg.NotFoundError{
+			Message: fmt.Sprintf("Not found exception: {%v} with id not found!", orderUpdateRequest.ID),
+		})
+	}
+
+	var order models.Order
+
+	// we can use automapper, but it will cause performance loss.
+	order.ID = orderUpdateRequest.ID
+	order.UserId = orderUpdateRequest.UserId
+	order.Product = orderUpdateRequest.Product
+	order.Total = orderUpdateRequest.Total
+
+	result, err := h.Service.Update(order)
+
+	if err != nil || result == false {
+		log.Printf("StatusInternalServerError: {%v} ", err.Error())
+		return c.JSON(http.StatusInternalServerError, pkg.InternalServerError{
+			Message: "Book cannot create! Something went wrong.",
+		})
+	}
+
+	// to response id and success boolean
+	jsonSuccessResultId := models.JSONSuccessResultId{
+		ID:      order.ID,
+		Success: result,
+	}
+
+	log.Printf("{%v} with id is updated.", jsonSuccessResultId.ID)
+	return c.JSON(http.StatusOK, jsonSuccessResultId)
 }
 
 // DeleteOrder godoc
