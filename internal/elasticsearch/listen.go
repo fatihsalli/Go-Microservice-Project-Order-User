6ea801e8e4f93b6c348254a2ac5638c27d0836cb
fatihsalli <mimsallifatih@gmail.com>
2023-03-11 17:07:14 +0300

    .

--- internal/elasticsearch/listen.go
+++ internal/elasticsearch/listen.go
@@ -2,19 +2,31 @@ package elasticsearch
 
 import (
 	"OrderUserProject/internal/kafka"
+	"OrderUserProject/internal/models"
+	"encoding/json"
 	"fmt"
 	"github.com/Shopify/sarama"
 )
 
 func ListenTopic() {
 	topic := "order-create"
-	worker, err := kafka.ConnectConsumer([]string{"localhost:9092"})
+	consumer, err := kafka.ConnectConsumer([]string{"localhost:9092"})
 
 	if err != nil {
 		panic(err)
 	}
 
-	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	ordersConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
 
-	fmt.Println(consumer.Messages())
+	// Start consuming messages
+	for msg := range ordersConsumer.Messages() {
+		order := &models.Order{}
+		if err := json.Unmarshal(msg.Value, order); err != nil {
+			fmt.Println("Failed to unmarshal order: ", err)
+		} else {
+			fmt.Printf("Received order: %+v\n", order)
+		}
+	}
+
+	return
 }
